0aa5a0a46f39154136a7db0013dfdb8777c5c3ef
shamir <mshamir732@gmail.com>
2024-09-17 09:52:46 +0530

    added dashboard details

--- vegefoods/AdminAuth/views.py
+++ vegefoods/AdminAuth/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.models import User
+from django.db.models import Sum,Count
+from order_app.models import Order,OrderItem
 
 def admin_login(request):
     if request.user.is_authenticated and request.user.is_staff:
@@ -19,7 +21,45 @@ def admin_login(request):
 def panel(request):
     if not request.user.is_authenticated or not request.user.is_superuser: 
         return redirect('admin_login')
-    return render(request, 'admin/dashboard.html')  
+    total_sales = Order.objects.aggregate(Sum('total_price'))['total_price__sum'] or 0
+    sales_count = Order.objects.count()
+    top_products = OrderItem.objects.values(
+    'product__product_name', 
+    'product__image_1', 
+    'product__available_stock',
+    'product__category__category_unit'
+    ).annotate(
+    total_quantity_sold=Sum('quantity'), 
+    total_revenue=Sum('subtotal_price'),
+
+    ).order_by('-total_quantity_sold')[:10]
+
+    orders_by_pincode = Order.objects.values(
+        'user__addresses__pincode',
+        'user__addresses__state',
+        'user__addresses__district'
+    ).annotate(
+        total_orders=Count('id')
+    ).order_by('-total_orders')
+    top_customers = (
+        User.objects
+        .filter(is_superuser=False)
+        .annotate(total_orders= Count('order'))
+        .annotate(total_spend=Sum('order__total_price'))
+        .filter(total_orders__gt=0) 
+        .order_by('-total_spend')[:10]
+    )
+    for i in top_products:
+        print("top products",i)
+
+    context={
+        'total_sales':total_sales,
+        'sales_count':sales_count,
+        'top_products':top_products,
+        'orders_by_pincode': orders_by_pincode,
+        'top_customers':top_customers
+    }
+    return render(request, 'admin/dashboard.html',context)  
 
  
 def user_managment(request): 
